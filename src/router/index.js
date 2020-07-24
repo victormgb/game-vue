7880f70e80f5e6ab57a5e8a8e3948fc74af6e5f6
victormgb <victormgb3099@gmail.com>
2020-07-24 17:15:16 -0400

    to the portafolio

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,46 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+Vue.use(Router)
+
+const routerOptions = [
+  { path: '/', name: 'Home' },
+  { path: '/region/:region/profile/:battleTag', name: 'Profile' },
+  { path: '/region/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
+  { path: '/about', name: 'About' },
+  { path: '/error', name: 'Error' },
+  { path: '*', redirect: { name: 'Home' } }
 ]
 
-const router = new VueRouter({
+const routes = routerOptions.map(r => {
+  return {
+    ...r,
+    // Lazy load
+    component: () => import(`@/views/${r.name}/Index.vue`)
+  }
+})
+
+//Para hacer chunks al momento del build de la app
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: () => import(/* webpackChunkName: "Home" */ '@/views/Home/Index.vue')
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     component: () => import(/* webpackChunkName: "About" */ '@/views/About/Index.vue')
+//   },
+//   {
+//     path: '/error',
+//     name: 'Error',
+//     component: () => import(/* webpackChunkName: "Error" */ '@/views/Error/Index.vue')
+//   }
+// ]
+
+const router = new Router({
   routes
 })
 
---
+++ src/views/Profile/Index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <BaseLoading v-if="isLoading"/>
+    <template v-if="profileData !== null">
+      <MainBlock :profile-data="profileData"/>
+      <ArtisansBlock :artisans-data="artisansData" />
+    </template>
+  </div>
+</template>
+
+<script>
+import BaseLoading from '@/components/BaseLoading'
+import MainBlock from './MainBlock/Index'
+import setError from '@/mixins/setError'
+import { getApiAccount } from '@/api/search'
+
+import ArtisansBlock from './ArtisansBlock/Index'
+
+export default {
+  name: 'ProfileView',
+  mixins: [
+    setError
+  ],
+  components: { BaseLoading,ArtisansBlock,MainBlock  },
+  data () {
+    return {
+      isLoading: false,
+      profileData: null
+    }
+  },
+  created () {
+    this.isLoading = true
+    const { region, battleTag: account } = this.$route.params
+    this.fetchData(region, account)
+  },
+  methods: {
+    /**
+     * Obtener los datos de la API
+     * Guardarlos en 'profileData'
+     */
+    fetchData (region, account) {
+      getApiAccount({ region, account })
+        .then(({ data }) => {
+          this.profileData = data
+        })
+        .catch((err) => {
+          this.profileData = null
+          const errObj = {
+            routeParams: this.$route.params,
+            message: err.message
+          }
+          if (err.response) {
+            errObj.data = err.response.data
+            errObj.status = err.response.status
+          }
+          this.setApiErr(errObj)
+          this.$router.push({ name: 'Error' })
+        })
+        .finally(() => {
+          this.isLoading = false
+        })
+    }
+  },
+  computed: {
+    artisansData () {
+      return {
+        blacksmith: this.profileData.blacksmith,
+        blacksmithHardcore: this.profileData.blacksmithHardcore,
+        jeweler: this.profileData.jeweler,
+        jewelerHardcore: this.profileData.jewelerHardcore,
+        mystic: this.profileData.mystic,
+        mysticHardcore: this.profileData.mysticHardcore
+      }
+    }
+  },
+}
+</script>
